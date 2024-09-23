6f8dcdf12a7b31d191a83dd81238a53aa2f62c53
vova3002 <98548237+vova3002@users.noreply.github.com>
2024-09-23 18:54:13 +0300

    STYLE

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 import "./js/eventGet.js";
 import "./js/eventHeader.js";
 import "./js/eventModal.js";
+import "./js/eventPagination.js";
--- js/eventGet.js
+++ js/eventGet.js
@@ -8,14 +8,30 @@ button.addEventListener("click", (event) => {
     const eventGet = data._embedded.events.map((mapEvent) => {
       console.log(mapEvent)
       const htmlEvent = `        <li class="event__item">
-      <img class="event__img" src="${mapEvent.images[3].url}">
+      <img class="event__img" src="${mapEvent.images[0].url}">
       <h3 name="eventH3" class="event__titles">${mapEvent.name}</h3>
       <p class="event__data">${mapEvent.dates.start.localDate}</p>
-      <p class="event__place">${mapEvent.promoter.description}</p>
+      <p class="event__place">${mapEvent.dates.timezone}</p>
     </li>`
     return htmlEvent
     })
-    eventList.innerHTML = eventGet
+    eventList.innerHTML = eventGet.join('')
+  })
+})
+button2.addEventListener("click", (event) => {
+  getFetch().then((data) => {
+    const eventGet = data._embedded.events.map((mapEvent) => {
+      console.log(mapEvent)
+      const htmlEvent = `        <li class="event__item">
+      <img class="event__img" src="${mapEvent.images[0].url}">
+      <h3 name="eventH3" class="event__titles">${mapEvent.name}</h3>
+      <p class="event__data">${mapEvent.dates.start.localDate}</p>
+      <p class="event__place">${mapEvent.dates.timezone}</p>
+    </li>`
+    return htmlEvent
+    })
+    eventList.innerHTML = eventGet.join('')
+    
   })
 })
 const getFetch = async () => {
@@ -33,5 +49,5 @@ getFetch().then((data) => {
   </li>`
   return htmlEvent
   })
-  eventList.innerHTML = eventGet
+  eventList.innerHTML = eventGet.join('');
 })
